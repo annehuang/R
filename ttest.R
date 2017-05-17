52343114055596a8d8c1c907b93c0f66426fbcac
Anne Huang <nehuang@gmail.com>
2017-05-16 21:45:14 -0500

    Create ttest.R

---
+++ ttest.R
@@ -0,0 +1,49 @@
+sink("ttests.txt", append=FALSE, split=FALSE)
+for (channel in c("FCz", "Fz", "T7", "T8", "FC2", "FC6", "Iz", "P7")){
+	for (task in c("M", "R", "N")){ 
+		print(paste(task, channel, sep=""))
+		#http://stackoverflow.com/questions/14420936/error-in-filefile-rt-invalid-description-argument-in-complete-cases-pro
+		path<-paste("C:", "/", task, channel, ".csv", sep="")
+		df<-read.csv(path, header= TRUE)
+		ttest<-t.test(df['DZ'], df['HC'])
+		print(ttest)
+	}
+}
+
+# betweenness
+for (channel in c("FCz", "Fz", "T7", "T8", "FC2", "FC6", "Iz", "P7")){
+	for (task in c("M", "R", "N")){ 
+		print(paste(task, channel, "bet", sep=""))
+		path<-paste("C:", "/", task, channel, "bet", ".csv", sep="")
+		df<-read.csv(path, header= TRUE)
+		ttest<-t.test(df['DZ'], df['HC'])
+		print(ttest)
+	}
+}
+
+# closeness
+for (channel in c("FCz", "Fz", "T7", "T8", "FC2", "FC6", "Iz", "P7")){
+	for (task in c("M", "R", "N")){ 
+		print(paste(task, channel, "close", sep=""))
+		path<-paste("C:", "/", task, channel, "close", ".csv", sep="")
+		df<-read.csv(path, header= TRUE)
+		ttest<-t.test(df['DZ'], df['HC'])
+		print(ttest)
+	}
+}
+
+for (task in c("M", "R", "N")){ 
+	print(paste(task, "AvgClustCoeffTTest", sep=""))
+	path<-paste("C:", "/", task, "AvgClustCoefficiency", ".csv", sep="")
+	df<-read.csv(path, header= TRUE)
+	ttest<-t.test(df['DZ'], df['HC'])
+	print(ttest)
+}
+
+for (task in c("M", "R", "N")){ 
+	print(paste(task, "ShortestPath", sep=""))
+	path<-paste("C:", "/", task, "ShortestPathLength", ".csv", sep="")
+	df<-read.csv(path, header= TRUE)
+	ttest<-t.test(df['DZ'], df['HC'])
+	print(ttest)
+}
